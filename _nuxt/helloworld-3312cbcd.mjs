837ec8ec1cde629e433980a63f3523d9e644d10d
dawnlion0305 <b1203380602@126.com>
2022-07-02 22:04:48 +0800

    init 6

--- _nuxt/helloworld-3312cbcd.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as r}from"./entry-699768c5.mjs";const c={};function l(n,t){return o(),r("div",null," hello ")}var _=e(c,[["render",l]]);export{_ as default};
--- _nuxt/index-3b39c0ae.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./entry-699768c5.mjs";const r={};function o(t,a){return c(),n("h2",null,"I'm index")}var s=e(r,[["render",o]]);export{s as default};
